5856831e09122c6793a1d88c73b580d0995b906f
g7o <kontakt@danielgattringer.com>
2015-12-10 14:40:18 +0100

    Krottis Pfusch ausgebessert
    Finde die Fehler

--- Client/Order.java
+++ Client/Order.java
@@ -1,17 +1,17 @@
 
 public class Order {
-	private int Runde;
-	private char kv;
-	private String Spieler;
-	private double limit;
-	private String Ware;
-	private int stk;
-	public Order(int Runde, char kv, String Spieler, double limit, String Ware, int stk){
-		this.Runde=Runde;
-		this.kv=kv;
-		this.Spieler=Spieler;
-		this.limit=limit;
-		this.Ware=Ware;
-		this.stk=stk;
-	}
+  private int runde;
+  private char kv;
+  private int spieler;
+  private double limit;
+  private int ware;
+  private int stk;
+  public Order(int runde, char kv, int spieler, double limit, int ware, int stk){
+    this.runde=runde;
+    this.kv=kv;
+    this.spieler=spieler;
+    this.limit=limit;
+    this.ware=ware;
+    this.stk=stk;
+  }
 }
