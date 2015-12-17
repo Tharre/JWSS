8e7d178eed54b39efdc6cd11b2f6f4e834302a7f
g7o <kontakt@danielgattringer.com>
2015-12-17 14:40:49 +0100

    Bla

---
+++ src/org/htl_hl/bibiProject/Common/Sell.java
@@ -0,0 +1,16 @@
+package org.htl_hl.bibiProject.Common;
+
+
+
+/**
+ * Created by Daniel on 17.12.2015.
+ */
+public class Sell {
+
+    public Sell(){}
+
+    public void sell(Order[] orders){
+
+    }
+
+}
