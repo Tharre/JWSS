166a24e36de963c0ffe6471bde90cf37e128301e
g7o <kontakt@danielgattringer.com>
2015-12-17 13:28:43 +0100

    Do host dei Calc.java

---
+++ logik/Calc.java
@@ -0,0 +1,31 @@
+    public void calculate(Order[] orders){
+      int amount=orders.length;
+      int highestQuantSell;
+      int indBestPrice=-1;
+      double price;
+      int[] quantitySells=new int[amount];
+      for(int i=0; i<=amount-1 ; i++){
+        price=orders[i].getLimit();
+        quantitySells[i]=orders[i].getQuantity();
+        if(orders[i].getIsBuy()==false){
+          for(int j=0; j<=amount-1; j++){
+            if(i==j){
+              continue;
+            }
+            if(orders[j].getLimit() > orders[i].getLimit() && orders[j].getIsBuy()==false){
+                quantitySells[i]=quantitySells[i]+orders[j].getQuantity();
+            }
+          }
+        }
+      }
+      highestQuantSell=quantitySells[0];
+      indBestPrice=0;
+      for(int k=0; k<=amount-1; k++){
+          if(highestQuantSell<quantitySells[k]){
+            highestQuantSell=quantitySells[k];
+            indBestPrice=k;
+          }
+      }
+      //Irgendwo bei irgendwas den Preis ändern
+      System.out.println("best: "+orders[indBestPrice].getLimit());
+  }
