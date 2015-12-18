b2184420e0352438e5dc6362fdd8087f13e8d30a
Tharre <tharre3@gmail.com>
2015-12-18 02:26:55 +0100

    Implementiere recaluateRate

--- src/org/htl_hl/bibiProject/Common/ExchangeRate.java
+++ src/org/htl_hl/bibiProject/Common/ExchangeRate.java
@@ -1,27 +1,68 @@
 package org.htl_hl.bibiProject.Common;
 
-/**
- * Created by Daniel on 17.12.2015.
- */
+import java.util.LinkedList;
+import java.util.List;
+
 public class ExchangeRate {
 
-    private Item item;
-    private double exchangeRate;
+    private final Item item;
+    private double rate;
 
-    public ExchangeRate(Item item, double exchangeRate){
-        this.item=item;
-        this.exchangeRate=exchangeRate;
+    public ExchangeRate() {
+        this(new Item(), 0.0);
     }
 
-    public Item getItem(){
+    public ExchangeRate(Item item, double rate) {
+        this.item = item;
+        this.rate = rate;
+    }
+
+    public Item getItem() {
         return item;
     }
 
-    public double getExchangeRate(){
-        return exchangeRate;
+    public double getRate() {
+        return rate;
     }
 
-    public void setExchangeRate(double exchangeRate){
-        this.exchangeRate=exchangeRate;
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public void recalculateRate(List<Order> orders) {
+        List<Double> possibleRates = new LinkedList<>();
+        double highestTurnover = 0;
+        double bestRate = 0;
+
+        // first fetch all possible exchange rates
+        for (Order order : orders) {
+            if (!order.getIsBuy()) // ignore sell orders
+                continue;
+
+            if (order.getLimit() == 0) // ignore 'best possible' orders
+                continue;
+
+            possibleRates.add(order.getLimit());
+        }
+
+        // then calculate the possible turnover at this rate
+        for (double possibleRate : possibleRates) {
+            int possibleTurnover = 0;
+            for (Order order : orders) {
+                if (order.getIsBuy()) // ignore buy orders
+                    continue;
+
+                if (order.getLimit() <= possibleRate) // all orders below the limit are satisfied
+                    possibleTurnover += order.getQuantity();
+            }
+
+            if (possibleTurnover > highestTurnover) {
+                bestRate = possibleRate;
+                highestTurnover = possibleTurnover;
+            }
+        }
+
+        // and now set the surviving, best rate
+        rate = bestRate;
     }
 }
