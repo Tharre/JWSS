354be1f7ee71c7a22267c3d12757f77875009eb2
g7o <kontakt@danielgattringer.com>
2015-12-18 07:33:30 +0100

    Prototyp Calculate
    Berechnet einfach Mittelwert aus höchstem und niedrigstem Preis in der
    Order List..

---
+++ src/org/htl_hl/bibiProject/Common/Calculate.java
@@ -0,0 +1,33 @@
+package org.htl_hl.bibiProject.Common;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Daniel on 17.12.2015.
+ */
+public class Calculate {
+
+    public Calculate(){};
+
+    public void calculate(List<Order> orders){
+        int length=orders.size()-1;
+        double readPrice;
+        double highestPrice;
+        double lowestPrice;
+        double helper;
+        double middlePrice;
+        highestPrice=orders.get(0).getLimit();
+        lowestPrice=orders.get(0).getLimit();
+        for(int i=1;i<=length;i++){
+            readPrice=orders.get(i).getLimit();
+            if(readPrice > highestPrice)
+                highestPrice=readPrice;
+            if(readPrice < lowestPrice)
+                lowestPrice=readPrice;
+        }
+        middlePrice=(highestPrice+lowestPrice)/2;
+        System.out.println(middlePrice);
+    }
+
+}
