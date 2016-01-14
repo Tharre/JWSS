30ae1f49cad58f4b0df3dc9f0f1d18b6c0f44540
g7o <kontakt@danielgattringer.com>
2016-01-14 20:24:19 +0100

    JKaufen JD

--- src/org/htl_hl/bibiProject/Client/JKaufen.java
+++ src/org/htl_hl/bibiProject/Client/JKaufen.java
@@ -8,8 +8,8 @@ import java.awt.event.*;
 import java.io.IOException;
 
 /**
- * <p>Title: Kaufen</p>
- * <p>Description:</p>
+ * <p>Title: JKaufen</p>
+ * <p>Description: In dieser Klasse befinden sich alle notwendigen Methoden und Eigenschaften der Klasse JKaufen.</p>
  * <p>Copyright: Copyright (c) 2016</p>
  * <p>Company: HTL Hollabrunn</p>
  * <br><br>
@@ -19,18 +19,45 @@ import java.io.IOException;
  * @version 0.1
  */
 public class JKaufen extends JPanel implements ActionListener {
-
+    /** btKauf - Private Eigenschaft der Klasse JKaufen vom Typ JButton.<br>
+     * Button mit dem Text "Bestellen".
+     */
     JButton btKauf = new JButton("Bestellen");
+    /** lblWare - Private Eigenschaft der Klasse JKaufen vom Typ JLabel.<br>
+     * Label mit dem Text "Name".
+     */
     JLabel lblWare = new JLabel("Name");
+    /** lblGewMenge - Private Eigenschaft der Klasse JKaufen vom Typ JLabel.<br>
+     * Label mit dem Text "gew. Menge: ".
+     */
     private JLabel lblGewMenge= new JLabel("gew. Menge: ");
+    /** lblMaxPreis - Private Eigenschaft der Klasse JKaufen vom Typ JLabel.<br>
+     * Label mit dem Text "max. Preis: ".
+     */
     private JLabel lblMaxPreis= new JLabel("max. Preis: ");
+    /** lblBlank - Private Eigenschaft der Klasse JKaufen vom Typ JLabel.<br>
+     *  Leeres Label aus optischen Gr&uuml;nden.
+     */
     private JLabel lblBlank = new JLabel("");
-
+    /** tfPreis - Private Eigenschaft der Klasse JVerkaufen vom Typ JTextField.<br>
+     * Eingabefeld f&uuml;r die gew&uuml;nschte Menge.
+     */
     private JTextField tfGewMenge= new JTextField("");
+    /** tfPreis - Private Eigenschaft der Klasse JVerkaufen vom Typ JTextField.<br>
+     * Eingabefeld f&uuml;r den maximalen Preis.
+     */
     private JTextField tfMaxPreis= new JTextField("");
-
+    /** ip - Private Eigenschaft der Klasse JKaufen vom Typ String.<br>
+     * IP-Adresse des Servers.
+     */
     private String ip;
+    /** game - Private Eigenschaft der Klasse JKaufen vom Typ Game.<br>
+     * Referenz auf das Game.
+     */
     private Game game;
+    /** player - Private Eigenschaft der Klasse JKaufen vom Typ Player.<br>
+     * Referenz auf den Player.
+     */
     private Player player;
 
     public JKaufen (String ip, Game game, Player player){
@@ -49,9 +76,10 @@ public class JKaufen extends JPanel implements ActionListener {
         this.game = game;
         this.player = player;
     }//JKaufen
-    /**
-     * Bearbeitung der Events der Buttons
-     * @param e Das Actionevent der Buttons
+
+    /** Methode zum Abarbeiten der Klick-Events von btKauf.<br>
+     * Eine Order wird an den Server gesendet und der Button-Text ge&auml;ndert.
+     * @param e ActionEvent - Klick-Event von btKauf
      */
     public void actionPerformed(ActionEvent e){
         if (e.getSource()== btKauf ) {
