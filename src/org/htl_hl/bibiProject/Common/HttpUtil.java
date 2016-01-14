69349346c1e397e117a21aa2f8f1f94fb2147e6d
g7o <kontakt@danielgattringer.com>
2016-01-14 10:26:56 +0100

    JD httpUtil

--- src/org/htl_hl/bibiProject/Common/HttpUtil.java
+++ src/org/htl_hl/bibiProject/Common/HttpUtil.java
@@ -7,10 +7,21 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/** Objekt vom Typ HttpUtil.
+ * In dieser Klasse befinden sich alle notwendigen Methoden und Eigenschaften von HttpUtil.
+ */
 public class HttpUtil {
 
     private static final String USER_AGENT = "JWSS Client/1.0";
 
+    /** Methode zum Eintragen von am Server Daten per HTTP-POST.
+     * @param ip String IP von Server
+     * @param res String Angeforderte Daten
+     * @param parameters String POST-Parameter
+     * @param typeClass Class<T> Hilfsvariable
+     * @return <T> Gibt angefordertes Objekt zurück
+     * @throws IOException
+     */
     public static <T> T sendPost(String ip, String res, String parameters, Class<T> typeClass) throws IOException {
         String url = ip + "/api/" + res;
         URL obj = new URL(url);
@@ -32,6 +43,13 @@ public class HttpUtil {
         return mapper.readValue(con.getInputStream(), typeClass);
     }
 
+    /** Methode zum Abrufen von Daten per HTTP-GET.
+     * @param ip String IP von Server
+     * @param res String Angeforderte Daten
+     * @param typeClass Class<T> Hilfsvariable
+     * @return <T> Gibt angefordertes Objekt zurück
+     * @throws IOException
+     */
     public static <T> T sendGet(String ip, String res, Class<T> typeClass) throws IOException {
         String url = ip + "/api/" + res + "/";
         URL obj = new URL(url);
